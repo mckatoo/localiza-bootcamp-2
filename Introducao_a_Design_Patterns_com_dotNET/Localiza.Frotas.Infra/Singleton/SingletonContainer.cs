4c24d44bb1274ed381ef5f2d80172c11a0552fae
Milton Carlos Katoo <mckatoo@gmail.com>
2022-01-18 20:23:16 -0300

    Introdução a Design Patterns com .Net - Singleton

---
+++ Introducao_a_Design_Patterns_com_dotNET/Localiza.Frotas.Infra/Singleton/SingletonContainer.cs
@@ -0,0 +1,7 @@
+namespace Localiza.Frotas.Infra.Singleton
+{
+  public class SingletonContainer
+  {
+    public Guid Id { get; } = Guid.NewGuid();
+  }
+}
---
+++ Introducao_a_Design_Patterns_com_dotNET/Localiza.Frotas/Controllers/SingletonController.cs
@@ -0,0 +1,24 @@
+using Localiza.Frotas.Infra.Singleton;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Localiza.Frotas.Controllers
+{
+  [Route("api/v1/[controller]")]
+  [ApiController]
+  public class SingletonController : ControllerBase
+  {
+    private readonly SingletonContainer singletonContainer;
+
+    public SingletonController(SingletonContainer singletonContainer)
+    {
+      this.singletonContainer = singletonContainer;
+    }
+    
+    [HttpGet()]
+    public IActionResult Get()
+    {
+      var singleton = singletonContainer;
+      return Ok(singleton);
+    }
+  }
+}
