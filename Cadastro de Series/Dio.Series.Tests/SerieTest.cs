0ffeeaacf6db5b3a2655e020e88bed362aaf5809
Milton Carlos Katoo <mckatoo@gmail.com>
2022-01-22 13:51:23 -0300

    Inicio da criação de testes de unidade.

---
+++ Cadastro de Series/Dio.Series.Tests/SerieTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dio.Series.Tests;
+
+[TestClass]
+public class SerieTest
+{
+    [TestMethod]
+    public void CreateSerie()
+    {
+        
+    }
+}
